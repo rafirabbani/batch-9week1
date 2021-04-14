fb2af6f97d6db3e1aa9a27e7288d5086ac82c920
Rafi Rabbani <rafirabbani103@gmail.com>
2021-04-14 19:24:41 +0700

    remove day1,day2 folder

--- day1/Hello.js
+++
@@ -1,3 +0,0 @@
-console.log("JS World");
-
-console.log("Hello")
--- day1/Number.js
+++
@@ -1,6 +0,0 @@
-const x = 12 / 5;
-const sisa = 12 % 5
-console.log(x, "\n", sisa);
-
-const y = Math.random() * 10;
-console.log(y)
